b2aec45550058b71c33827b9ac2f293a9497414d
MarianoSgherza <marianosgherza@gmail.com>
2019-09-09 05:26:35 -0300

    Prueba para Snoop

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+    var fotos = document.getElementById("fotos-carrusel");
+
+    var avanzar = document.querySelector(".crsl-container");
+    var forward = document.querySelector(".adelante");
+
+ 
+    avanzar.addEventListener("click", function () {
+        
+        var primero = document.getElementById("fotos-carrusel").firstChild;
+        fotos.removeChild(primero);
+        fotos.appendChild(primero);
+  
+        
+      
+    });
+
+    forward.addEventListener("click", function () {
+        
+        var primero = document.getElementById("fotos-carrusel").firstChild;
+        fotos.removeChild(primero);
+        fotos.appendChild(primero);
+  
+        
+      
+    });
+
+    $(".hamburger").click(function () {
+        $("#menu").slideToggle(300);
+    })
+    
+})
